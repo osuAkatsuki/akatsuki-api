45252490845f7eee73d6f26f682f5442b24e6910
cmyui <cmyuiosu@gmail.com>
2024-05-04 03:15:33 -0400

    Remove raven & gocertifi deps

--- common/method_data.go
+++ common/method_data.go
@@ -3,10 +3,8 @@ package common
 import (
 	"encoding/json"
 	"runtime"
-	"strconv"
 	"strings"
 
-	"github.com/getsentry/raven-go"
 	"github.com/jmoiron/sqlx"
 	"github.com/valyala/fasthttp"
 	"golang.org/x/exp/slog"
@@ -42,18 +40,13 @@ func (md MethodData) ClientIP() string {
 // Err logs an error. If RavenClient is set, it will use the client to report
 // the error to sentry, otherwise it will just write the error to stdout.
 func (md MethodData) Err(err error) {
-	user := &raven.User{
-		ID:       strconv.Itoa(md.User.UserID),
-		Username: md.User.Value,
-		IP:       md.Ctx.RemoteAddr().String(),
-	}
 	// Generate tags for error
 	tags := map[string]string{
 		"endpoint": string(md.Ctx.RequestURI()),
 		"token":    md.User.Value,
 	}
 
-	_err(err, tags, user, md.Ctx)
+	_err(err, tags, md.Ctx)
 }
 
 // Err for peppy API calls
@@ -63,62 +56,27 @@ func Err(c *fasthttp.RequestCtx, err error) {
 		"endpoint": string(c.RequestURI()),
 	}
 
-	_err(err, tags, nil, c)
+	_err(err, tags, c)
 }
 
 // WSErr is the error function for errors happening in the websockets.
 func WSErr(err error) {
 	_err(err, map[string]string{
 		"endpoint": "/api/v1/ws",
-	}, nil, nil)
+	}, nil)
 }
 
 // GenericError is just an error. Can't make a good description.
 func GenericError(err error) {
-	_err(err, nil, nil, nil)
+	_err(err, nil, nil)
 }
 
-func _err(err error, tags map[string]string, user *raven.User, c *fasthttp.RequestCtx) {
+func _err(err error, tags map[string]string, c *fasthttp.RequestCtx) {
 	_, file, no, ok := runtime.Caller(2)
 	if ok {
 		slog.Error("An error occurred", "filename", file, "line", no, "error", err.Error())
 	} else {
 		slog.Error("An error occurred", "error", err.Error())
-	}
-}
-
-func generateRavenHTTP(ctx *fasthttp.RequestCtx) *raven.Http {
-	if ctx == nil {
-		return nil
-	}
-
-	// build uri
-	uri := ctx.URI()
-	// safe to use b2s because a new string gets allocated eventually for
-	// concatenation
-	sURI := b2s(uri.Scheme()) + "://" + b2s(uri.Host()) + b2s(uri.Path())
-
-	// build header map
-	// using ctx.Request.Header.Len would mean calling .VisitAll two times
-	// which can be quite expensive since it means iterating over all the
-	// headers, so we give a rough estimate of the number of headers we expect
-	// to have
-	m := make(map[string]string, 16)
-	ctx.Request.Header.VisitAll(func(k, v []byte) {
-		// not using b2s because we mustn't keep references to the underlying
-		// k and v
-		m[string(k)] = string(v)
-	})
-
-	return &raven.Http{
-		URL: sURI,
-		// Not using b2s because raven sending is concurrent and may happen
-		// AFTER the request, meaning that values could potentially be replaced
-		// by new ones.
-		Method:  string(ctx.Method()),
-		Query:   string(uri.QueryString()),
-		Cookies: string(ctx.Request.Header.Peek("Cookie")),
-		Headers: m,
 	}
 }
 
